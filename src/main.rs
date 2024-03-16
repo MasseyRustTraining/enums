613303f9e1ace9642ce6b7bd8901de5fb69e7a43
Bart Massey <bart.massey@gmail.com>
2024-03-15 17:23:44 -0700

    added a bunch of extra stuff

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,27 @@ enum S {
 }
 */
 
+// This is zero-overhead because niche optimization.
+// 8 bytes on 64-bit machine.
+pub enum RR<'a> {
+    Ref64(&'a u64),
+    Ref32(&'a u32),
+}
+
 #[derive(Debug, Clone, Copy)]
-#[repr(u8)]
+pub struct Dud;
+
+#[derive(Debug, Clone, Copy)]
 pub enum Color {
     Rgb([u8; 3]),
     Rgba([u8; 4]),
     Monochrome(bool),
-    Gray { scale: u8, alpha: u8 },
+    Gray { scale: u8, alpha: u8, dud: Dud },
 }
 use Color::*;
+
+pub static SBLACK: Color = Rgb([0, 0, 0]);
+pub const CBLACK: Color = Rgb([0, 0, 0]);
 
 impl Color {
     fn show_color(self) {
@@ -31,6 +43,8 @@ impl Color {
             println!();
         }
 
+        let s = 5;
+        let a = 7;
         match self {
             Rgb(components) => print_components(&components),
             Rgba(components) => print_components(&components),
@@ -39,8 +53,11 @@ impl Color {
             } else {
                 println!("000000")
             },
-            Gray { scale: 0, alpha: 0 } => println!("transparent"),
-            Gray { scale: s, alpha: a } => {
+            Gray { scale: 0, alpha: 0, .. } => println!("transparent"),
+            Gray { scale: s0, alpha: a0, .. } if s == s0 && a == a0 => {
+                println!("special");
+            }
+            Gray { scale: s, alpha: a, .. } => {
                 let rgba = [s, s, s, a];
                 print_components(&rgba);
             }
@@ -48,11 +65,13 @@ impl Color {
     }
 }
 
-fn main() {
+pub fn tuple_fn((_x, _y): (u32, u32)) {
+}
 
+fn main() {
     let c = Color::Rgb([17, 0, 255]);
     c.show_color();
 
-    let c = Color::Gray { scale: 0, alpha: 0 };
+    let c = Color::Gray { scale: 0, alpha: 0, dud: Dud };
     c.show_color();
 }
